01e1564e899f1d13425b1295dab908e2fa7f7241
Pierre Thiollent <pierre.thiollent76@gmail.com>
2022-01-14 10:38:40 +0100

    Add authentication tests

---
+++ tests/Functional/AuthTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Tests\Functional;
+
+class AuthTest extends AbstractTest
+{
+    /**
+     * @throws \JsonException
+     */
+    public function testAuthWithWrongCredentialsShouldReturn401(): void
+    {
+        $this->client->request(
+            'POST',
+            '/api/login',
+            server: ['CONTENT_TYPE' => 'application/json'],
+            content: '{"email":"admin","password":"admin"}'
+        );
+
+        self::assertResponseStatusCodeSame(401);
+        $response = $this->client->getResponse();
+        self::assertJson($response->getContent());
+
+        $data = json_decode($response->getContent(), true, 512, JSON_THROW_ON_ERROR);
+        self::assertArrayHasKey('code', $data);
+        self::assertArrayHasKey('message', $data);
+
+        self::assertEquals('Invalid credentials.', $data['message']);
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    public function testAuthWithCorrectCredentialsShouldReturn200(): void
+    {
+        $this->client->request(
+            'POST',
+            '/api/login',
+            server: ['CONTENT_TYPE' => 'application/json'],
+            content: '{"email":"pierre.thiollent76@gmail.com","password":"test"}'
+        );
+
+        self::assertResponseStatusCodeSame(200);
+        $response = $this->client->getResponse();
+        self::assertJson($response->getContent());
+
+        $data = json_decode($response->getContent(), true, 512, JSON_THROW_ON_ERROR);
+        self::assertArrayHasKey('token', $data);
+    }
+}
