c002c02d76423772721a252731ecd85c0e486696
Pierre Thiollent <pierre.thiollent76@gmail.com>
2022-01-14 14:17:40 +0100

    Remove unused hashPassword method into PostUserEventSubscriber

--- src/Events/PostUsersSubscriber.php
+++ src/Events/PostUsersSubscriber.php
@@ -9,14 +9,12 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\Security;
 
 class PostUsersSubscriber implements EventSubscriberInterface
 {
     public function __construct(
         private Security $security,
-        public UserPasswordHasherInterface $passwordHasher
     ) {
     }
 
@@ -26,7 +24,6 @@ class PostUsersSubscriber implements EventSubscriberInterface
         return [
             KernelEvents::VIEW => [
                 ['currentClientForUsers', EventPriorities::POST_VALIDATE],
-                ['hashPassword', EventPriorities::POST_VALIDATE],
             ],
         ];
     }
@@ -38,16 +35,6 @@ class PostUsersSubscriber implements EventSubscriberInterface
 
         if ($user instanceof User && Request::METHOD_POST === $method) {
             $user->setClient($this->security->getUser());
-        }
-    }
-
-    public function hashPassword(ViewEvent $viewEvent): void
-    {
-        $user = $viewEvent->getControllerResult();
-        $method = $viewEvent->getRequest()->getMethod();
-
-        if ($user instanceof User && Request::METHOD_POST === $method) {
-            $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPassword()));
         }
     }
 }
